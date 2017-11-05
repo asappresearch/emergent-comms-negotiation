95478d706d88e914d7081675b88661adce4e46be
Hugh Perkins <hp@asapp.com>
2017-11-05 20:14:56 +0000

    more batched reward tests

--- test_rewards.py
+++ test_rewards.py
@@ -239,7 +239,7 @@ def test_single_game_term_t2():
 
     agent = 0 if t  % 2 == 0 else 1
     term = torch.ByteTensor([1])
-    alive_games = [{'rewards': [0, 0]}]
+    alive_games = [{'rewards': [0.0, 0.0]}]
     ecn.calc_rewards(s=s, t=t, prosocial=prosocial, alive_games=alive_games, agent=agent, term=term)
     assert alive_games[0]['rewards'] == [ratio, ratio]
 
@@ -265,6 +265,9 @@ def test_single_game_term_t2_batch3():
     # since only one terminated, reward should be for simply the hard-coded ones above
     # all others should be zero
 
+    s.pool[0] = s.last_proposal[0]
+    s.pool[2] = s.last_proposal[2]
+
     total_available = 3 * 5 + 7 * 4 + 2 * 5
     print('total_available', total_available)
     # so, the  proposer is the second agent, ie agent 1
@@ -279,8 +282,225 @@ def test_single_game_term_t2_batch3():
     agent = 0 if t  % 2 == 0 else 1
     alive_games = []
     for b in range(batch_size):
-        alive_games.append({'rewards': [0, 0]})
+        alive_games.append({'rewards': [0.0, 0.0]})
     ecn.calc_rewards(s=s, t=t, prosocial=prosocial, alive_games=alive_games, agent=agent, term=term)
     assert alive_games[0]['rewards'] == [0.0, 0.0]
     assert alive_games[1]['rewards'] == [ratio, ratio]
     assert alive_games[2]['rewards'] == [0.0, 0.0]
+
+
+def test_single_game_term_t2_batch3_2term():
+    t = 2
+    prosocial = True
+    batch_size = 3
+    torch.manual_seed(123)
+    np.random.seed(123)
+    s = ecn.State(batch_size=batch_size)
+
+    s.pool = torch.from_numpy(np.random.choice(10, (batch_size, 3))).long()
+    s.pool[1] = torch.LongTensor([3, 7, 2])
+
+    s.utilities = torch.from_numpy(np.random.choice(10, (batch_size, 2, 3))).long()
+    s.utilities[1] = torch.LongTensor([[5,4,3], [3,4,5]])
+
+    s.last_proposal = torch.from_numpy(np.random.choice(10, (batch_size, 3))).long()
+    s.last_proposal[1] = torch.LongTensor([3, 0, 0])
+
+    term = torch.ByteTensor([0, 1, 1])
+    # second should have reward too, and shouldnt affect the reward we are calcing
+
+    s.pool[0] = s.last_proposal[0]
+    s.pool[2] = s.last_proposal[2]
+
+    total_available = 3 * 5 + 7 * 4 + 2 * 5
+    print('total_available', total_available)
+    # so, the  proposer is the second agent, ie agent 1
+    # so, the proposer, agent 1, will take: 3 0 0
+    # accepter, agent 0, will take 0 7 2
+    actual = 0 * 5 + 7 * 4 + 2 * 3 + \
+            3 * 3
+    print('actual', actual)
+    ratio = actual / total_available
+    print('ratio', ratio)
+
+    agent = 0 if t  % 2 == 0 else 1
+    alive_games = []
+    for b in range(batch_size):
+        alive_games.append({'rewards': [0.0, 0.0]})
+    ecn.calc_rewards(s=s, t=t, prosocial=prosocial, alive_games=alive_games, agent=agent, term=term)
+    assert alive_games[0]['rewards'] == [0.0, 0.0]
+    assert alive_games[1]['rewards'] == [ratio, ratio]
+    assert alive_games[2]['rewards'] != [0.0, 0.0]
+
+
+def test_single_game_term_t2_batch3_2termb():
+    t = 2
+    prosocial = True
+    batch_size = 3
+    torch.manual_seed(123)
+    np.random.seed(123)
+    s = ecn.State(batch_size=batch_size)
+
+    s.pool = torch.from_numpy(np.random.choice(10, (batch_size, 3))).long()
+    s.pool[1] = torch.LongTensor([3, 7, 2])
+
+    s.utilities = torch.from_numpy(np.random.choice(10, (batch_size, 2, 3))).long()
+    s.utilities[1] = torch.LongTensor([[5,4,3], [3,4,5]])
+
+    s.last_proposal = torch.from_numpy(np.random.choice(10, (batch_size, 3))).long()
+    s.last_proposal[1] = torch.LongTensor([3, 0, 0])
+
+    term = torch.ByteTensor([1, 1, 0])
+    # zeroth should have reward too, and shouldnt affect the reward we are calcing
+    s.pool[0] = s.last_proposal[0]
+    s.pool[2] = s.last_proposal[2]
+
+    total_available = 3 * 5 + 7 * 4 + 2 * 5
+    print('total_available', total_available)
+    # so, the  proposer is the second agent, ie agent 1
+    # so, the proposer, agent 1, will take: 3 0 0
+    # accepter, agent 0, will take 0 7 2
+    actual = 0 * 5 + 7 * 4 + 2 * 3 + \
+            3 * 3
+    print('actual', actual)
+    ratio = actual / total_available
+    print('ratio', ratio)
+
+    agent = 0 if t  % 2 == 0 else 1
+    alive_games = []
+    for b in range(batch_size):
+        alive_games.append({'rewards': [0.0, 0.0]})
+    ecn.calc_rewards(s=s, t=t, prosocial=prosocial, alive_games=alive_games, agent=agent, term=term)
+    assert alive_games[0]['rewards'] != [0.0, 0.0]
+    assert alive_games[1]['rewards'] == [ratio, ratio]
+    assert alive_games[2]['rewards'] == [0.0, 0.0]
+
+
+def test_single_game_term_t2_batch3_3term():
+    t = 2
+    prosocial = True
+    batch_size = 3
+    torch.manual_seed(123)
+    np.random.seed(123)
+    s = ecn.State(batch_size=batch_size)
+
+    s.pool = torch.from_numpy(np.random.choice(10, (batch_size, 3))).long()
+    s.pool[1] = torch.LongTensor([3, 7, 2])
+
+    s.utilities = torch.from_numpy(np.random.choice(10, (batch_size, 2, 3))).long()
+    s.utilities[1] = torch.LongTensor([[5,4,3], [3,4,5]])
+
+    s.last_proposal = torch.from_numpy(np.random.choice(10, (batch_size, 3))).long()
+    s.last_proposal[1] = torch.LongTensor([3, 0, 0])
+
+    term = torch.ByteTensor([1, 1, 1])
+    # zeroth should have reward too, and shouldnt affect the reward we are calcing
+
+    s.pool[0] = s.last_proposal[0]
+    s.pool[2] = s.last_proposal[2]
+
+    total_available = 3 * 5 + 7 * 4 + 2 * 5
+    print('total_available', total_available)
+    # so, the  proposer is the second agent, ie agent 1
+    # so, the proposer, agent 1, will take: 3 0 0
+    # accepter, agent 0, will take 0 7 2
+    actual = 0 * 5 + 7 * 4 + 2 * 3 + \
+            3 * 3
+    print('actual', actual)
+    ratio = actual / total_available
+    print('ratio', ratio)
+
+    agent = 0 if t  % 2 == 0 else 1
+    alive_games = []
+    for b in range(batch_size):
+        alive_games.append({'rewards': [0.0, 0.0]})
+    ecn.calc_rewards(s=s, t=t, prosocial=prosocial, alive_games=alive_games, agent=agent, term=term)
+    assert alive_games[0]['rewards'] != [0.0, 0.0]
+    assert alive_games[1]['rewards'] == [ratio, ratio]
+    assert alive_games[2]['rewards'] != [0.0, 0.0]
+
+
+def test_single_game_term_t2_batch3_zero_term():
+    t = 2
+    prosocial = True
+    batch_size = 3
+    torch.manual_seed(123)
+    np.random.seed(123)
+    s = ecn.State(batch_size=batch_size)
+
+    s.pool = torch.from_numpy(np.random.choice(10, (batch_size, 3))).long()
+    s.pool[1] = torch.LongTensor([3, 7, 2])
+
+    s.utilities = torch.from_numpy(np.random.choice(10, (batch_size, 2, 3))).long()
+    s.utilities[1] = torch.LongTensor([[5,4,3], [3,4,5]])
+
+    s.last_proposal = torch.from_numpy(np.random.choice(10, (batch_size, 3))).long()
+    s.last_proposal[1] = torch.LongTensor([3, 0, 0])
+
+    term = torch.ByteTensor([0, 0, 0])
+
+    s.pool[0] = s.last_proposal[0]
+    s.pool[2] = s.last_proposal[2]
+
+    total_available = 3 * 5 + 7 * 4 + 2 * 5
+    print('total_available', total_available)
+    # so, the  proposer is the second agent, ie agent 1
+    # so, the proposer, agent 1, will take: 3 0 0
+    # accepter, agent 0, will take 0 7 2
+    actual = 0 * 5 + 7 * 4 + 2 * 3 + \
+            3 * 3
+    print('actual', actual)
+    ratio = actual / total_available
+    print('ratio', ratio)
+
+    agent = 0 if t  % 2 == 0 else 1
+    alive_games = []
+    for b in range(batch_size):
+        alive_games.append({'rewards': [0.0, 0.0]})
+    ecn.calc_rewards(s=s, t=t, prosocial=prosocial, alive_games=alive_games, agent=agent, term=term)
+    assert alive_games[0]['rewards'] == [0.0, 0.0]
+    assert alive_games[1]['rewards'] == [0.0, 0.0]
+    assert alive_games[2]['rewards'] == [0.0, 0.0]
+
+
+def test_single_game_term_t2_batch3_oneth_not_term():
+    t = 2
+    prosocial = True
+    batch_size = 3
+    torch.manual_seed(123)
+    np.random.seed(123)
+    s = ecn.State(batch_size=batch_size)
+
+    s.pool = torch.from_numpy(np.random.choice(10, (batch_size, 3))).long()
+    s.pool[1] = torch.LongTensor([3, 7, 2])
+
+    s.utilities = torch.from_numpy(np.random.choice(10, (batch_size, 2, 3))).long()
+    s.utilities[1] = torch.LongTensor([[5,4,3], [3,4,5]])
+
+    s.last_proposal = torch.from_numpy(np.random.choice(10, (batch_size, 3))).long()
+    s.last_proposal[1] = torch.LongTensor([3, 0, 0])
+
+    term = torch.ByteTensor([1, 0, 1])
+
+    s.pool[0] = s.last_proposal[0]
+    s.pool[2] = s.last_proposal[2]
+
+    total_available = 3 * 5 + 7 * 4 + 2 * 5
+    print('total_available', total_available)
+    # so, the  proposer is the second agent, ie agent 1
+    # so, the proposer, agent 1, will take: 3 0 0
+    # accepter, agent 0, will take 0 7 2
+    actual = 0 * 5 + 7 * 4 + 2 * 3 + \
+            3 * 3
+    print('actual', actual)
+    ratio = actual / total_available
+    print('ratio', ratio)
+
+    agent = 0 if t  % 2 == 0 else 1
+    alive_games = []
+    for b in range(batch_size):
+        alive_games.append({'rewards': [0.0, 0.0]})
+    ecn.calc_rewards(s=s, t=t, prosocial=prosocial, alive_games=alive_games, agent=agent, term=term)
+    assert alive_games[0]['rewards'] != [0.0, 0.0]
+    assert alive_games[1]['rewards'] == [0.0, 0.0]
+    assert alive_games[2]['rewards'] != [0.0, 0.0]
