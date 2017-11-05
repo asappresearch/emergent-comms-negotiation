0d839bb12c02dbfd250109a28ae879800cfacddd
Hugh Perkins <hp@asapp.com>
2017-11-05 20:06:46 +0000

    batched reward test

--- test_ecn.py
+++ test_ecn.py
@@ -315,3 +315,46 @@ def test_single_game_term_t2():
     alive_games = [{'rewards': [0, 0]}]
     ecn.calc_rewards(s=s, t=t, prosocial=prosocial, alive_games=alive_games, agent=agent, term=term)
     assert alive_games[0]['rewards'] == [ratio, ratio]
+
+
+def test_single_game_term_t2_batch3():
+    t = 2
+    prosocial = True
+    batch_size = 3
+    torch.manual_seed(123)
+    np.random.seed(123)
+    s = ecn.State(batch_size=batch_size)
+
+    s.pool = torch.from_numpy(np.random.choice(10, (batch_size, 3))).long()
+    s.pool[1] = torch.LongTensor([3, 7, 2])
+
+    s.utilities = torch.from_numpy(np.random.choice(10, (batch_size, 2, 3))).long()
+    s.utilities[1] = torch.LongTensor([[5,4,3], [3,4,5]])
+
+    s.last_proposal = torch.from_numpy(np.random.choice(10, (batch_size, 3))).long()
+    s.last_proposal[1] = torch.LongTensor([3, 0, 0])
+
+    term = torch.ByteTensor([0, 1, 0])
+    # since only one terminated, reward should be for simply the hard-coded ones above
+    # all others should be zero
+
+    total_available = 3 * 5 + 7 * 4 + 2 * 5
+    print('total_available', total_available)
+    # so, the  proposer is the second agent, ie agent 1
+    # so, the proposer, agent 1, will take: 3 0 0
+    # accepter, agent 0, will take 0 7 2
+    actual = 0 * 5 + 7 * 4 + 2 * 3 + \
+            3 * 3
+    print('actual', actual)
+    ratio = actual / total_available
+    print('ratio', ratio)
+
+    agent = 0 if t  % 2 == 0 else 1
+    # print('term', term)
+    alive_games = []
+    for b in range(batch_size):
+        alive_games.append({'rewards': [0, 0]})
+    ecn.calc_rewards(s=s, t=t, prosocial=prosocial, alive_games=alive_games, agent=agent, term=term)
+    assert alive_games[0]['rewards'] == [0.0, 0.0]
+    assert alive_games[1]['rewards'] == [ratio, ratio]
+    assert alive_games[2]['rewards'] == [0.0, 0.0]
